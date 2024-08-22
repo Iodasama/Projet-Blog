c7e436dfa3740545150808de5560e9e0918b9d37
Jerome Trezeguet <jerome.trezeguet@lapiscine.pro>
2024-08-22 18:24:53 +0200

    HomePage

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -20,16 +20,6 @@ class ArticleRepository extends ServiceEntityRepository
 //    /**
 //     * @return Article[] Returns an array of Article objects
 //     */
-    public function findArticlesHome(): array
-    {
-        return $this->createQueryBuilder('a')
-
-            ->orderBy('a.createdAt', 'DESC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
 
 
 //    Guest function findOneBySomeField($value): ?Article
